e34422a4765b90bd6784f390b7c5d706b7bc085a
saikeerthana-123 <63954676+saikeerthana-123@users.noreply.github.com>
2021-04-06 19:40:31 +0530

    Add files via upload

---
+++ project_102_automation.py
@@ -0,0 +1,24 @@
+print("Five steps for a successful project")
+step = input("whats step number- ")
+if(step == '1'):
+    print("Step 1: Starting")
+    print("Every project starts somewhere. Decide the theme for your project")
+
+elif(step == '2'):
+    print("Step 2: Negotiation.")
+    print("Once you've got your ideas, you need to make sure they are acceptable to your intended audience (usually your teacher)")
+
+elif(step == '3'):
+    print("Step 3: Time and Resources")
+    print("By this point, you (and your group) will probably be working with only the lightest supervision, so you really need to pay attention to what you actually need to do, and when you need to do it by.")
+
+elif(step == '4'):
+    print("Step 4: Outcome")
+    print("After all your work, you will, eventually, finish your project, hopefully to the pleased surprise of your teacher. Take a moment to be proud of yourself and your colleagues.")
+
+elif(step == '5'):
+    print("Step 5: Judgement")
+    print(" As the philosopher says, failure is always an option. But, so is success. Once you have presented your final piece of work, it will be judged. Hopefully, you will have succeeded")
+
+if(step >= '6'):
+    print("Sorry! There are only 5 steps.")
